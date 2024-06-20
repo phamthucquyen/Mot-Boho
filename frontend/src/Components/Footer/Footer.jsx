91ab858266546b440fb649a82abad796d7e3cc6e
Bianca Pham <qpham@mail.wlu.edu>
2024-06-20 19:19:05 +0700

    formatting LoginSignup Page

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
         <div className='footer'>
             <div className="footer-logo">
                 <img src={footer_logo} alt="" />
-                <p>SHOPPER</p>
+                <p>MÓT BOHO</p>
             </div>
             <ul className='footer-links'>
                 <li>Company</li>
