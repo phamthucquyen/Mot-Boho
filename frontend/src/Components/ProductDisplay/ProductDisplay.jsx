6836c4765c6b2349217c60c55637237dee88ffe4
Bianca Pham <qpham@mail.wlu.edu>
2024-06-20 21:05:20 +0700

    add product-display-right

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -18,7 +18,7 @@ const ProductDisplay = (props) => {
                     <img className='productdisplay-main-img' src={product.image} alt="" />
                 </div>
             </div>
-            <div className='productdisplay-left'>
+            <div className='productdisplay-right'>
                 <h1>{product.name}</h1>
                 <div className='productdisplay-right-star'>
                     <img src={star_icon} alt="" />
